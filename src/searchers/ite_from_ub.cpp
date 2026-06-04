8f62f7a0a1fb23c0edec9c80b5760336fee8a864
HieuHayHo <truongxuanhieu11@gmail.com>
2026-06-04 11:53:51 +0700

    refactor with to label

--- src/searchers/ite_from_ub.cpp
+++ src/searchers/ite_from_ub.cpp
@@ -5,9 +5,9 @@
 
 IteFromUB::IteFromUB(int target_value, int lower_bound, int upper_bound) : Iteration(target_value, lower_bound, upper_bound)
 {
-    search_order = create_search_order();
+    label_search_order = create_label_search_order();
     std::cout << "c [Main] Search order:";
-    for (const auto &width : search_order)
+    for (const auto &width : label_search_order)
     {
         std::cout << " " << width;
     }
@@ -16,7 +16,7 @@ IteFromUB::IteFromUB(int target_value, int lower_bound, int upper_bound) : Itera
 
 IteFromUB::~IteFromUB() {}
 
-std::deque<int> IteFromUB::create_search_order()
+std::deque<int> IteFromUB::create_label_search_order()
 {
     std::deque<int> order_result;
     for (int i = upper_bound; i >= lower_bound; i--)
--- src/searchers/iteration.cpp
+++ src/searchers/iteration.cpp
@@ -13,8 +13,8 @@
 
 Iteration::Iteration(int target_value, int lower_bound, int upper_bound) : Searcher(target_value, lower_bound, upper_bound)
 {
-    max_width_SAT = lower_bound - 1;
-    min_width_UNSAT = upper_bound + 1;
+    highest_label_SAT = lower_bound - 1;
+    lowest_label_UNSAT = upper_bound + 1;
 }
 
 Iteration::~Iteration()
@@ -30,14 +30,14 @@ void Iteration::encode_and_solve()
 
     for (int i = 0; i < GlobalData::worker_count && i < (upper_bound + 1 - lower_bound); i += 1)
     {
-        int next_width_to_search = get_next_width_to_search();
-        if (next_width_to_search <= lower_bound - 1 || next_width_to_search >= upper_bound + 1)
+        int next_label_to_search = get_next_label_to_search();
+        if (next_label_to_search <= lower_bound - 1 || next_label_to_search >= upper_bound + 1)
         {
-            break; // No valid width to search
+            break; // No valid label to search
         }
         else
         {
-            create_work_pid(next_width_to_search);
+            create_work_pid(next_label_to_search);
         }
     }
 
@@ -71,9 +71,9 @@ void Iteration::encode_and_solve()
                     switch (WEXITSTATUS(status))
                     {
                     case 10:
-                        if (it->first > max_width_SAT)
+                        if (it->first > highest_label_SAT)
                         {
-                            max_width_SAT = it->first;
+                            highest_label_SAT = it->first;
                             std::cout << "c [Main] Max width SAT is set to " << it->first << ".\n";
                         }
 
@@ -88,9 +88,9 @@ void Iteration::encode_and_solve()
                         }
                         break;
                     case 20:
-                        if (it->first < min_width_UNSAT)
+                        if (it->first < lowest_label_UNSAT)
                         {
-                            min_width_UNSAT = it->first;
+                            lowest_label_UNSAT = it->first;
                             std::cout << "c [Main] Min width UNSAT is set to " << it->first << "\n";
                         }
 
@@ -109,7 +109,7 @@ void Iteration::encode_and_solve()
                     }
 
                     work_pids.erase(it);
-                    if (work_pids.empty() && search_order.empty() && kill(lim_pid, 0) == 0)
+                    if (work_pids.empty() && label_search_order.empty() && kill(lim_pid, 0) == 0)
                     {
                         kill(lim_pid, SIGTERM);
                     }
@@ -156,18 +156,18 @@ void Iteration::encode_and_solve()
             fflush(stdout);
             while (int(work_pids.size()) < GlobalData::worker_count)
             {
-                int next_width_to_seach = get_next_width_to_search();
-                if (next_width_to_seach <= lower_bound - 1 || next_width_to_seach >= upper_bound + 1)
+                int next_label_to_seach = get_next_label_to_search();
+                if (next_label_to_seach <= lower_bound - 1 || next_label_to_seach >= upper_bound + 1)
                 {
-                    break; // No valid width to search
+                    break; // No valid label to search
                 }
                 else
                 {
-                    create_work_pid(next_width_to_seach);
+                    create_work_pid(next_label_to_seach);
                 }
             }
 
-            if (work_pids.empty() && search_order.empty())
+            if (work_pids.empty() && label_search_order.empty())
             {
                 kill(lim_pid, SIGTERM);
             }
@@ -180,8 +180,8 @@ void Iteration::encode_and_solve()
 
     std::cout << "r [Main] \n";
     std::cout << "r [Main] Final results: \n";
-    std::cout << "r [Main] Max width SAT:  \t" << ((max_width_SAT == lower_bound - 1) ? "-" : std::to_string(max_width_SAT)) << ".\n";
-    std::cout << "r [Main] Min width UNSAT:\t" << ((min_width_UNSAT == upper_bound + 1) ? "-" : std::to_string(min_width_UNSAT)) << ".\n";
+    std::cout << "r [Main] Max width SAT:  \t" << ((highest_label_SAT == lower_bound - 1) ? "-" : std::to_string(highest_label_SAT)) << ".\n";
+    std::cout << "r [Main] Min width UNSAT:\t" << ((lowest_label_UNSAT == upper_bound + 1) ? "-" : std::to_string(lowest_label_UNSAT)) << ".\n";
     std::cout << "r [Main] Total real time: " << encode_duration << " ms.\n";
     std::cout << "r [Main] Total memory consumed: " << *max_consumed_memory << " MB.\n";
     std::cout << "r [Main] \n";
@@ -196,7 +196,7 @@ void Iteration::encode_and_print_dimacs()
     }
 };
 
-void Iteration::create_work_pid(int width)
+void Iteration::create_work_pid(int label)
 {
     // std::cout << "p PID: " << getpid() << ", PPID: " << getppid() << ".\n";
     pid_t pid = fork();
@@ -204,16 +204,16 @@ void Iteration::create_work_pid(int width)
 
     if (pid < 0)
     {
-        std::cerr << "e [w = " << width << "] Fork failed!\n";
+        std::cerr << "e [w = " << label << "] Fork failed!\n";
         exit(-1);
     }
     else if (pid == 0)
     {
         prctl(PR_SET_PDEATHSIG, SIGTERM);
-        std::cout << "c [w = " << width << "] Start task in PID: " << getpid() << ".\n";
+        std::cout << "c [w = " << label << "] Start task in PID: " << getpid() << ".\n";
 
         // Child process: perform the task
-        int result = do_work_pid_task(width);
+        int result = do_work_pid_task(label);
 
         exit(result);
     }
@@ -221,39 +221,38 @@ void Iteration::create_work_pid(int width)
     {
         // Parent process stores the child's PID
         // std::cout << "c Child pid " << width << " - " << pid << " is tracked in PID: " << getpid() << ".\n";
-        work_pids[width] = pid;
+        work_pids[label] = pid;
     }
 }
 
-int Iteration::do_work_pid_task(int width)
+int Iteration::do_work_pid_task(int label)
 {
     // Dynamically allocate and use ABPEncoder in child process
-    IteInstance instance(width);
+    IteInstance instance(label);
 
     int result = instance.encode_and_solve_problem();
 
-    std::cout << "c [w = " << width << "] Result: " << result << ".\n";
+    std::cout << "c [w = " << label << "] Result: " << result << ".\n";
 
-    // std::cout << "c [w = " << width << "] Child " << width << " completed task.\n";
     return result;
 }
 
-int Iteration::get_next_width_to_search()
+int Iteration::get_next_label_to_search()
 {
-    if (search_order.empty())
+    if (label_search_order.empty())
         return lower_bound - 1; // To terminate the search if no valid width is left. Value (upper_bound + 1) also works.
 
-    int next_width = search_order.front();
-    search_order.pop_front();
+    int next_label = label_search_order.front();
+    label_search_order.pop_front();
 
-    while (next_width <= max_width_SAT || next_width >= min_width_UNSAT)
+    while (next_label <= highest_label_SAT || next_label >= lowest_label_UNSAT)
     {
-        if (search_order.empty())
+        if (label_search_order.empty())
             return lower_bound - 1; // To terminate the search if no valid width is left. Value (upper_bound + 1) also works.
 
-        next_width = search_order.front();
-        search_order.pop_front();
+        next_label = label_search_order.front();
+        label_search_order.pop_front();
     }
 
-    return next_width;
+    return next_label;
 }
