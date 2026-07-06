ab128e045293f4439ecce98fcc7eb3d9554c902b
Hieu Hay Ho <truongxuanhieu11@gmail.com>
2026-07-06 02:48:03 +0000

    run split size

--- experiments/run_files/run_all.sh
+++ experiments/run_files/run_all.sh
@@ -5,43 +5,38 @@ export TZ='Asia/Ho_Chi_Minh'
 scripts=(
     # "incremental/extended_bound/ite_abp_x1.5.sh"
     # "incremental/extended_bound/ite_abp_x1.25.sh"
-
     # "incremental/reduced_bound/ite_abp_x0.5.sh"
     # "incremental/reduced_bound/ite_abp_x0.75.sh"
-
     # "incremental/standard_bound/ite_abp_x0.5.sh"
     # "incremental/standard_bound/ite_abp_x0.75.sh"
     # "incremental/standard_bound/ite_abp_x1.sh"
 
-
-
+    "incremental-split-clause/reduced_bound/incre_abp_x0.5-10.sh"
     # "incremental-split-clause/reduced_bound/incre_abp_x0.75-0.sh"
     # "incremental-split-clause/reduced_bound/incre_abp_x0.75-5.sh"
-    # "incremental-split-clause/reduced_bound/incre_abp_x0.75-10.sh"
-
-
+    "incremental-split-clause/reduced_bound/incre_abp_x0.75-10.sh"
 
     # "iteration-bfs/extended_bound/ite_abp_x1.5.sh"
     # "iteration-bfs/extended_bound/ite_abp_x1.25.sh"
-
     # "iteration-bfs/reduced_bound/ite_abp_x0.5.sh"
     # "iteration-bfs/reduced_bound/ite_abp_x0.75.sh"
-
     # "iteration-bfs/standard_bound/ite_abp_x0.5.sh"
     # "iteration-bfs/standard_bound/ite_abp_x0.75.sh"
     # "iteration-bfs/standard_bound/ite_abp_x1.sh"
 
-
+    "iteration-bfs-split-clause/reduced_bound/ite_abp_x0.5-10.sh"
+    "iteration-bfs-split-clause/reduced_bound/ite_abp_x0.75-10.sh"
 
     # "iteration-from-ub/extended_bound/ite_abp_x1.5.sh"
     # "iteration-from-ub/extended_bound/ite_abp_x1.25.sh"
-    
     # "iteration-from-ub/reduced_bound/ite_abp_x0.5.sh"
     # "iteration-from-ub/reduced_bound/ite_abp_x0.75.sh"
-
     # "iteration-from-ub/standard_bound/ite_abp_x0.5.sh"
     # "iteration-from-ub/standard_bound/ite_abp_x0.75.sh"
     # "iteration-from-ub/standard_bound/ite_abp_x1.sh"
+
+    "iteration-from-ub-split-clause/reduced_bound/ite_abp_x0.5-10.sh"
+    "iteration-from-ub-split-clause/reduced_bound/ite_abp_x0.75-10.sh"
 )
 
 chmod +x "${scripts[@]}"
