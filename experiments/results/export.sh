e75042117355d86fa29ec08694e69942de51f539
Hieu Hay Ho <truongxuanhieu11@gmail.com>
2026-08-28 07:34:48 +0000

    remove redundant searching config

--- experiments/results/export.sh
+++ experiments/results/export.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 python3 export_incremental.py   ./abp/incremental               ./abp/incremental/abp-sat-incremental.xlsx
-python3 export_iteration.py     ./abp/iteration-bfs             ./abp/iteration-bfs/abp-sat-iteration-bfs.xlsx
 python3 export_iteration.py     ./abp/iteration-bfs-reverse     ./abp/iteration-bfs-reverse/abp-sat-iteration-bfs-reverse.xlsx
-python3 export_iteration.py     ./abp/iteration-from-ub         ./abp/iteration-from-ub/abp-sat-iteration-from-ub.xlsx
 
-python3 export_iteration.py     ./cabp/iteration-bfs            ./cabp/iteration-bfs/cabp-sat-iteration-bfs.xlsx
 python3 export_iteration.py     ./cabp/iteration-bfs-reverse    ./cabp/iteration-bfs-reverse/cabp-sat-iteration-bfs-reverse.xlsx
-python3 export_iteration.py     ./cabp/iteration-from-ub        ./cabp/iteration-from-ub/cabp-sat-iteration-from-ub.xlsx
